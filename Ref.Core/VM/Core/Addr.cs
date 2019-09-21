193c77d89a796747aa9ddb7b5b5fc8dad2b7680f
Furesoft <filmee24@gmail.com>
2019-09-21 19:51:29 +0200

    added Addr struct

---
+++ Ref.Core/VM/Core/Addr.cs
@@ -0,0 +1,19 @@
+﻿namespace Ref.Core.VM.Core
+{
+    public struct Addr
+    {
+        public Addr(int addr, Heap h)
+        {
+            this._value = addr;
+            this._heap = h;
+        }
+
+        public object Dereference()
+        {
+            _heap.storage.Find(_value).;
+        }
+
+        private Heap _heap { get; set; }
+        private int _value { get; set; }
+    }
+}
