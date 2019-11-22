5fb8cd2e282156d74250d372b0127212b5617d48
Furesoft <filmee24@gmail.com>
2019-11-22 16:15:55 +0100

    bugfix popregister

--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -10,13 +10,18 @@ namespace TestConsole
         private static void Main(string[] args)
         {
             var ass = new CommandWriter();
-            ass.Add(OpCode.MOV, (int)Registers.A, 0xC00FFEE);
+            ass.Add(OpCode.MOV, (int)Registers.A, 0x2A);
             ass.Add(OpCode.MOV, (int)Registers.B, 0x2A);
-            ass.Add(OpCode.PUSH, (int)Registers.B);
+            ass.Add(OpCode.ADD, (int)Registers.A, (int)Registers.B);
+            ass.Add(OpCode.PUSH, (int)Registers.ACC);
             ass.Add(OpCode.PRINT);
 
             var vm = new VirtualMachine();
             vm.Run(ass.Save());
+
+            vm.Stack.PushRegisters(vm.Register);
+            vm.Register.ClearRegister(Registers.A);
+            vm.Stack.PopRegisters(vm.Register);
 
             Console.WriteLine("Register: " + vm.ViewMemoryOf<Register>().ToHex());
             Console.WriteLine("Stack: " + vm.ViewMemoryOf<Stack>().ToHex());
