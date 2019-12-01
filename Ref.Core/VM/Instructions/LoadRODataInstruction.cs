ed5d305e683c3c9417e8d3eb52e4b752319b996f
Furesoft <filmee24@gmail.com>
2019-12-01 09:38:03 +0100

    added load data to register from ro data section

---
+++ Ref.Core/VM/Instructions/LoadRODataInstruction.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Ref.Core.Parser;
+using Ref.Core.VM.IO;
+
+namespace Ref.Core.VM.Instructions
+{
+    internal class LoadRODataInstruction : Instruction
+    {
+        public override OpCode OpCode => OpCode.LOADRO;
+
+        public override void Invoke(AsmCommand cmd, VirtualMachine vm)
+        {
+            var startIndex = (int)cmd[0];
+            var reg = (Registers)(int)cmd[1];
+
+            var section = vm.Assembly[AssemblySections.ReadOnly];
+            var value = BitConverter.ToInt32(section.Raw, startIndex);
+
+            vm.Register[reg] = value;
+        }
+    }
+}
