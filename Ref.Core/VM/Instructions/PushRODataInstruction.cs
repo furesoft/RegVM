5bc9efdd1bc07150b442f02f8b7933115dc4fa07
Furesoft <filmee24@gmail.com>
2019-12-01 09:41:11 +0100

    added push data from ro data section

---
+++ Ref.Core/VM/Instructions/PushRODataInstruction.cs
@@ -0,0 +1,21 @@
+﻿using Ref.Core.Parser;
+using Ref.Core.VM.IO;
+using System;
+
+namespace Ref.Core.VM.Instructions
+{
+    internal class PushRODataInstruction : Instruction
+    {
+        public override OpCode OpCode => OpCode.PUSHRO;
+
+        public override void Invoke(AsmCommand cmd, VirtualMachine vm)
+        {
+            var startIndex = (int)cmd[0];
+
+            var section = vm.Assembly[AssemblySections.ReadOnly];
+            var value = BitConverter.ToInt32(section.Raw, startIndex);
+
+            vm.Stack.Push(value);
+        }
+    }
+}
