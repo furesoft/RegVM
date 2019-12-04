ff1db356c9dfafef8dce175e8ecd5821d2293992
Furesoft <filmee24@gmail.com>
2019-12-04 11:12:43 +0100

    implemented video device initialisation

--- Ref.Core/VM/IO/Devices/VideoDevice.cs
+++ Ref.Core/VM/IO/Devices/VideoDevice.cs
@@ -1,12 +1,20 @@
 ﻿using System;
+using System.Drawing;
 using System.Runtime.InteropServices;
 using Ref.Core.VM.Core.Ports;
 
 namespace Ref.Core.VM.IO.Devices
 {
     [Port(0xFFAF, PortAccess.Write)] // control port
+    [Port(0xFFAA, PortAccess.Write)] // Init Port (Size)
+    [Port(0xFFAB, PortAccess.Write)] // Init Port (Location)
     public class VideoDevice : IPortMappedDevice
     {
+        public static void CleanUP()
+        {
+            ReleaseDC(IntPtr.Zero, desktopPtr);
+        }
+
         public static void Enable_ConsoleMode()
         {
             if (!AttachConsole(ATTACH_PARENT_PROCESS))
@@ -15,11 +23,16 @@ namespace Ref.Core.VM.IO.Devices
             }
         }
 
-        public static void Enable_VideoMode()
+        public static void Enable_VideoMode(Rectangle bgRec)
         {
             FreeConsole();
 
             //draw to screen
+            desktopPtr = GetDC(IntPtr.Zero);
+            var g = Graphics.FromHdc(desktopPtr);
+
+            //draw background
+            g.FillRectangle(Brushes.Black, bgRec);
         }
 
         public static void Write(char c)
@@ -30,7 +43,7 @@ namespace Ref.Core.VM.IO.Devices
             }
             catch
             {
-                throw new RuntimeException("Cant write to Console in VideoMode");
+                //throw new RuntimeException("Cant write to Console in VideoMode");
             }
         }
 
@@ -41,19 +54,40 @@ namespace Ref.Core.VM.IO.Devices
 
         public void HandleWrite(int port, int value, VirtualMachine vm)
         {
-            switch (value)
+            if (port == 0xFFAF)
             {
-                case 0:
-                    Enable_ConsoleMode();
-                    break;
+                switch (value)
+                {
+                    case 0:
+                        Enable_ConsoleMode();
+                        break;
 
-                case 1:
-                    Enable_VideoMode();
-                    break;
+                    case 1:
+                        Enable_VideoMode(bgRec);
+                        break;
+                }
+            }
+            else if (port == 0xFFAA)
+            {
+                byte[] bytes = BitConverter.GetBytes(value);
+                short width = BitConverter.ToInt16(bytes, 0);
+                short height = BitConverter.ToInt16(bytes, 2);
+
+                bgRec = new Rectangle(bgRec.Location, new Size(width, height));
+            }
+            else if (port == 0xFFAB)
+            {
+                byte[] bytes = BitConverter.GetBytes(value);
+                short x = BitConverter.ToInt16(bytes, 0);
+                short y = BitConverter.ToInt16(bytes, 2);
+
+                bgRec = new Rectangle(new Point(x, y), bgRec.Size);
             }
         }
 
         private const uint ATTACH_PARENT_PROCESS = 0x0ffffffff;
+        private static IntPtr desktopPtr;
+        private Rectangle bgRec;
 
         [DllImport("kernel32")]
         private static extern bool AllocConsole();
@@ -63,5 +97,11 @@ namespace Ref.Core.VM.IO.Devices
 
         [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
         private static extern bool FreeConsole();
+
+        [DllImport("User32.dll")]
+        private static extern IntPtr GetDC(IntPtr hwnd);
+
+        [DllImport("User32.dll")]
+        private static extern void ReleaseDC(IntPtr hwnd, IntPtr dc);
     }
 }
