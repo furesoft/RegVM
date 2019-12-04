348ea21e91d4457d2c1c6410a595d401b5903ae3
Furesoft <filmee24@gmail.com>
2019-12-04 16:42:46 +0100

    moved drawing to videobuffer class

---
+++ Ref.Core/VM/Core/VideoBuffer.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+
+namespace Ref.Core.VM.Core
+{
+    public class VideoBuffer : IDisposable
+    {
+        public int this[int x, int y]
+        {
+            get
+            {
+                return _bufferData[x + (y * _rec.Width)];
+            }
+            set
+            {
+                _bufferData[x + (y * _rec.Width)] = value;
+            }
+        }
+
+        public static VideoBuffer Create(Rectangle rec)
+        {
+            var buf = new VideoBuffer();
+            buf._rec = rec;
+            buf._bufferData = new int[rec.Width * rec.Height * 4];
+
+            desktopPtr = GetDesktopWindow();
+            buf._graphics = Graphics.FromHwnd(desktopPtr);
+
+            return buf;
+        }
+
+        public void Clear()
+        {
+            for (int i = 0; i < _bufferData.Length; i++)
+            {
+                _bufferData[i] = 0x000000;
+            }
+        }
+
+        public void Dispose()
+        {
+            ReleaseDC(IntPtr.Zero, desktopPtr);
+        }
+
+        public void Flush()
+        {
+            //ToDo: implement Flush Method on VideoBuffer to draw buffer on screen
+        }
+
+        private static IntPtr desktopPtr;
+        private int[] _bufferData;
+        private Graphics _graphics;
+        private Rectangle _rec;
+
+        [DllImport("user32.dll", SetLastError = false)]
+        private static extern IntPtr GetDesktopWindow();
+
+        [DllImport("User32.dll")]
+        private static extern void ReleaseDC(IntPtr hwnd, IntPtr dc);
+    }
+}
--- Ref.Core/VM/IO/Devices/VideoDevice.cs
+++ Ref.Core/VM/IO/Devices/VideoDevice.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Drawing;
 using System.Runtime.InteropServices;
+using Ref.Core.VM.Core;
 using Ref.Core.VM.Core.Ports;
 using Ref.Core.VM.IO.MappedIO;
 
@@ -12,9 +13,11 @@ namespace Ref.Core.VM.IO.Devices
     [AddressRange(0xFFFF, 0xFFFFFF)] // screen buffer range
     public class VideoDevice : IPortMappedDevice, IMemoryMappedDevice
     {
+        public static VideoBuffer Buffer;
+
         public static void CleanUP()
         {
-            ReleaseDC(IntPtr.Zero, desktopPtr);
+            Buffer.Dispose();
         }
 
         public static void Enable_ConsoleMode()
@@ -29,12 +32,9 @@ namespace Ref.Core.VM.IO.Devices
         {
             FreeConsole();
 
-            //draw to screen
-            desktopPtr = GetDC(IntPtr.Zero);
-            graphics = Graphics.FromHdc(desktopPtr);
-
-            //draw background
-            graphics.FillRectangle(Brushes.Black, bgRec);
+            Buffer = VideoBuffer.Create(bgRec);
+            Buffer.Clear();
+            Buffer.Flush();
         }
 
         public static void Write(char c)
@@ -55,7 +55,7 @@ namespace Ref.Core.VM.IO.Devices
             short x = BitConverter.ToInt16(bytes, 0);
             short y = BitConverter.ToInt16(bytes, 2);
 
-            graphics.DrawRectangle(new Pen(Color.FromArgb(value), 1), new Rectangle(new Point(x + bgRec.X, y + bgRec.Y), new Size(10, 10))); //Todo: change to real pixel
+            Buffer[x, y] = value;
         }
 
         public void HandleRead(int port, Registers reg, VirtualMachine vm)
@@ -97,8 +97,7 @@ namespace Ref.Core.VM.IO.Devices
         }
 
         private const uint ATTACH_PARENT_PROCESS = 0x0ffffffff;
-        private static IntPtr desktopPtr;
-        private static Graphics graphics;
+
         private Rectangle bgRec;
 
         [DllImport("kernel32")]
@@ -109,11 +108,5 @@ namespace Ref.Core.VM.IO.Devices
 
         [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
         private static extern bool FreeConsole();
-
-        [DllImport("User32.dll")]
-        private static extern IntPtr GetDC(IntPtr hwnd);
-
-        [DllImport("User32.dll")]
-        private static extern void ReleaseDC(IntPtr hwnd, IntPtr dc);
     }
 }
