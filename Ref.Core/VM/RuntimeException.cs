b5df2c2fa503ec452a71ffdb8486fa4ebff9dab8
Furesoft <filmee24@gmail.com>
2019-12-04 10:47:42 +0100

    added runtimeexception

---
+++ Ref.Core/VM/RuntimeException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Ref.Core
+{
+    [Serializable]
+    internal class RuntimeException : Exception
+    {
+        public RuntimeException()
+        {
+        }
+
+        public RuntimeException(string message) : base(message)
+        {
+        }
+
+        public RuntimeException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected RuntimeException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
