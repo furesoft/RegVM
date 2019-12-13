89da751cefe12464921214dcb8c83aa12d38657f
Furesoft <filmee24@gmail.com>
2019-12-13 19:19:53 +0100

    added Logger

---
+++ Ref.Core/Logger.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+
+namespace Ref.Core
+{
+    public static class Logger
+    {
+        public static void Log(string message)
+        {
+            File.AppendAllText(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\vm.txt", $"{DateTime.Now}: {message}\n\r");
+        }
+    }
+}
