42cf855c0817bf4badeccf3b652f4f4530a4126a
Furesoft <filmee24@gmail.com>
2019-12-14 19:25:31 +0100

    added exit application interrupt
    to wait for input from stack

---
+++ Ref.Core/VM/IO/InterruptHandlers/ExitApplicationInterrupt.cs
@@ -0,0 +1,15 @@
+﻿using Ref.Core.VM.Core.Interrupts;
+using System;
+
+namespace Ref.Core.VM.IO.InterruptHandlers
+{
+    [Interrupt(0x1)]
+    internal class ExitApplicationInterruptHandler : IInterruptHandler
+    {
+        public void Handle(VirtualMachine vm)
+        {
+            var errCode = vm.Stack.Pop();
+            Environment.Exit(errCode);
+        }
+    }
+}
