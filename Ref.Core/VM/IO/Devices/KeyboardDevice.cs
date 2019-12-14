7fe3fcc4008980be1e506471c47b47d8d2a3de7d
Furesoft <filmee24@gmail.com>
2019-12-14 19:25:40 +0100

    added core for keyboard device

---
+++ Ref.Core/VM/IO/Devices/KeyboardDevice.cs
@@ -0,0 +1,20 @@
+﻿using Ref.Core.VM.Core.Ports;
+using Ref.Core.VM.IO.MappedIO;
+
+namespace Ref.Core.VM.IO.Devices
+{
+    [Port(0xBCD, PortAccess.Write)] //Control Port
+    [Port(0xBCD1, PortAccess.Read)] // Data Access Port - isInputAvailable
+    internal class KeyboardDevice : IPortMappedDevice
+    {
+        public void HandleRead(int port, Registers reg, VirtualMachine vm)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void HandleWrite(int port, int value, VirtualMachine vm)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
