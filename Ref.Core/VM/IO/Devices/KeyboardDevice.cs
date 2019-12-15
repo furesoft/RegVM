39d9722ee8d51a29cab8116e1aa539b22c27a407
Furesoft <filmee24@gmail.com>
2019-12-15 16:54:04 +0100

    added keyboard hook

--- Ref.Core/VM/IO/Devices/KeyboardDevice.cs
+++ Ref.Core/VM/IO/Devices/KeyboardDevice.cs
@@ -7,24 +7,44 @@ namespace Ref.Core.VM.IO.Devices
     [Port(0xBCD1, PortAccess.Read)] // Data Access Port - isInputAvailable
     internal class KeyboardDevice : IPortMappedDevice
     {
+        public VirtualMachine VM { get; set; }
+
         public KeyboardDevice()
         {
-            //ToDo: init GlobalSystemKeyboard hook, when new input is available push it on stack
+            _globalKeyboardHook = new GlobalKeyboardHook();
+            _globalKeyboardHook.KeyboardPressed += OnKeyPressed;
         }
 
-        public void HandleRead(int port, Registers reg, VirtualMachine vm)
+        public void Dispose()
+        {
+            _globalKeyboardHook?.Dispose();
+        }
+
+        public void HandleRead(int port, Registers reg)
         {
             if (port == 0xBCD1)
             {
-                vm.Register[Registers.KDR] = isInputAvailable ? 1 : 0;
+                VM.Register[Registers.KDS] = isInputAvailable ? 1 : 0;
+                isInputAvailable = false;
             }
         }
 
-        public void HandleWrite(int port, int value, VirtualMachine vm)
+        public void HandleWrite(int port, int value)
         {
             throw new System.NotImplementedException();
         }
 
+        private GlobalKeyboardHook _globalKeyboardHook;
+
         private bool isInputAvailable = false;
+
+        private void OnKeyPressed(object sender, GlobalKeyboardHookEventArgs e)
+        {
+            if (e.KeyboardData.VirtualCode != GlobalKeyboardHook.VkSnapshot)
+                return;
+            isInputAvailable = true;
+
+            VM.Register[Registers.KDR] = e.KeyboardData.VirtualCode;
+        }
     }
 }
