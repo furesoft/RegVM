7b39ac2a27026529fc3309c1ded1e55399dab673
Furesoft <filmee24@gmail.com>
2019-12-16 16:52:01 +0100

    moved to new assembly format in runner

--- Ref-Runner/Program.cs
+++ Ref-Runner/Program.cs
@@ -1,8 +1,10 @@
-﻿using Ref.Core;
+﻿using LibObjectFile.Elf;
+using Ref.Core;
 using Ref.Core.VM.IO;
 using Ref.Core.VM.IO.Devices;
 using System.IO;
 using System.Linq;
+using TestConsole;
 
 namespace RefVM_Runner
 {
@@ -11,10 +13,11 @@ namespace RefVM_Runner
         private static void Main(string[] args)
         {
             var filename = args.First();
-            var ass = Assembly.Load(File.ReadAllBytes(filename));
+            var ass = ElfObjectFile.Read(File.Open(filename, FileMode.OpenOrCreate));
             var vm = new VirtualMachine(ass);
 
             VideoDevice.Enable_ConsoleMode();
+            VideoDevice.DefaultContext = new WinformsDrawingContext();
 
             vm.Run();
 
