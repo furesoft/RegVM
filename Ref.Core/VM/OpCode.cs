5006ae0ba639f4f9dd74d798af56404f155451ce
Furesoft <filmee24@gmail.com>
2019-12-26 17:37:24 +0100

    cleanup opcodes

--- Ref.Core/VM/OpCode.cs
+++ Ref.Core/VM/OpCode.cs
@@ -2,39 +2,55 @@
 {
     public enum OpCode
     {
-        INC,
-        DEC,
+        NOP = 0x00,
+        HLT = 0x01,
 
-        MOV,
+        LOAD8 = 0x4,
+        LOAD16 = 0x5,
+        LOAD32 = 0x6,
 
-        LOADRO,
-        PUSHRO,
+        LOADRO8 = 0x7,
+        LOADRO16 = 0x8,
+        LOADRO32 = 0x9,
 
-        ADD,
-        DIV,
-        MUL,
-        SUB,
+        MOV = 0xB,
 
-        NOP,
-        JMPR,
-        CMP,
-        PUSH,
-        POP,
-        JMP,
-        LOAD,
-        CALL,
-        RET,
-        OUT,
-        OUTR,
-        IN,
-        PUSHL,
-        JMPE,
-        JMPNE,
-        EQUAL,
-        NEQUAL,
-        INT,
+        JMP = 0xD,
+        JMPR = 0xE,
+        JMPE = 0xF,
+        JMPNE = 0x10,
 
-        NEW,
-        FREE,
+        PUSH8 = 0x13,
+        PUSH16 = 0x14,
+        PUSH32 = 0x15,
+
+        POP = 0x19,
+
+        CMP = 0x1B,
+
+        INT = 0x1D,
+
+        OUT8 = 0x1F,
+        OUT16 = 0x20,
+        OUT32 = 0x21,
+
+        IN = 0x25,
+
+        NEW = 0x2B,
+        FREE = 0x2C,
+
+        CALL = 0x2E,
+        RET = 0x2F,
+
+        ADD = 0x34,
+        SUB = 0x35,
+        MUL = 0x36,
+        DIV = 0x37,
+
+        INC = 0x38,
+        DEC = 0x39,
+
+        EQUAL = 0x3C,
+        NEQUAL = 0x3C,
     }
 }
