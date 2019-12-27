1f6ddd194f7a01b82b643148aea43ccf34176a45
Furesoft <filmee24@gmail.com>
2019-12-27 20:43:05 +0100

    added sub instruction

---
+++ Ref.Core/VM/Instructions/Arithmetic/SubInstruction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Ref.Core.Parser;
+
+namespace Ref.Core.VM.Instructions
+{
+    internal class SubInstruction : Instruction
+    {
+        public override OpCode OpCode => OpCode.SUB;
+
+        public override void Invoke(AsmCommand cmd, VirtualMachine vm)
+        {
+            var add_left = vm.Register[(Registers)(int)cmd[0]];
+            var add_right = vm.Register[(Registers)(int)cmd[1]];
+
+            vm.Register[Registers.ACC] = add_left - add_right;
+        }
+    }
+}
