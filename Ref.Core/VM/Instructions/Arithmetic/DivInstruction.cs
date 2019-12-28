fa6dfadeecb3e87029a3a050505065fb11c92f6b
Furesoft <filmee24@gmail.com>
2019-12-28 09:32:42 +0100

    added instructions

--- Ref.Core/VM/Instructions/Arithmetic/DivInstruction.cs
+++ Ref.Core/VM/Instructions/Arithmetic/DivInstruction.cs
@@ -2,16 +2,17 @@
 
 namespace Ref.Core.VM.Instructions
 {
-    internal class SubInstruction : Instruction
+    internal class DivInstruction : Instruction
     {
-        public override OpCode OpCode => OpCode.SUB;
+        public override OpCode OpCode => OpCode.DIV;
 
         public override void Invoke(AsmCommand cmd, VirtualMachine vm)
         {
             var add_left = vm.Register[(Registers)(int)cmd[0]];
             var add_right = vm.Register[(Registers)(int)cmd[1]];
 
-            vm.Register[Registers.ACC] = add_left - add_right;
+            vm.Register[Registers.ACC] = add_left / add_right;
+            vm.Register[Registers.REM] = add_left % add_right;
         }
     }
 }
--- Ref.Core/VM/Instructions/Arithmetic/MulInstruction.cs
+++ Ref.Core/VM/Instructions/Arithmetic/MulInstruction.cs
@@ -2,16 +2,16 @@
 
 namespace Ref.Core.VM.Instructions
 {
-    internal class SubInstruction : Instruction
+    internal class MulInstruction : Instruction
     {
-        public override OpCode OpCode => OpCode.SUB;
+        public override OpCode OpCode => OpCode.MUL;
 
         public override void Invoke(AsmCommand cmd, VirtualMachine vm)
         {
             var add_left = vm.Register[(Registers)(int)cmd[0]];
             var add_right = vm.Register[(Registers)(int)cmd[1]];
 
-            vm.Register[Registers.ACC] = add_left - add_right;
+            vm.Register[Registers.ACC] = add_left * add_right;
         }
     }
 }
