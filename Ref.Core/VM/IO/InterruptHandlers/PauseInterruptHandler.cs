b9f349ac18f8c530b70dc0466ddbca15fcc7896a
Furesoft <filmee24@gmail.com>
2019-12-28 09:32:31 +0100

    added pause interrupt for console apps

---
+++ Ref.Core/VM/IO/InterruptHandlers/PauseInterruptHandler.cs
@@ -0,0 +1,14 @@
+﻿using Ref.Core.VM.Core.Interrupts;
+using System;
+
+namespace Ref.Core.VM.IO.InterruptHandlers
+{
+    [Interrupt(0x6A03E)]
+    public class PauseInterruptHandler : IInterruptHandler
+    {
+        public void Handle(VirtualMachine vm)
+        {
+            Console.ReadLine();
+        }
+    }
+}
