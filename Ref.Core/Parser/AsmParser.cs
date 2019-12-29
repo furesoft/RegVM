1b81d387982b2ac115431b4a35f0a8242fc3b8f2
Furesoft <filmee24@gmail.com>
2019-12-29 10:13:53 +0100

    added label parsing to parser

--- Ref.Core/Parser/AsmParser.cs
+++ Ref.Core/Parser/AsmParser.cs
@@ -8,6 +8,8 @@ namespace Ref.Core.Parser
         {
             var res = new AsmFile();
             var lines = src.Split(new char[] { '\r', '\n' }, System.StringSplitOptions.RemoveEmptyEntries);
+            bool inLabel = false;
+            AsmLabel label = null;
 
             foreach (var line in lines)
             {
@@ -19,11 +21,40 @@ namespace Ref.Core.Parser
 
                     res.DataCommands.Add(datacmd);
                 }
+                else if (line.EndsWith(":")) // line is label definition
+                {
+                    var name = line.Substring(0, line.Length - 1);
+                    inLabel = true;
+                    label = new AsmLabel(name);
+                }
                 else //when line is a opcode
                 {
-                    var opline = line.Split(' ');
-                    var opcmd = SyntaxNode.CreateCommand(opline[0], ParseArgs(line.Substring(opline[0].Length + 1)));
-                    res.Commands.Add(opcmd);
+                    if (!line.StartsWith("\t") && label != null)
+                    {
+                        inLabel = false;
+                        //ToDo: add label to result
+                        res.Labels.Add(label);
+                        label = null;
+                    }
+
+                    List<AsmCommand> cmds = res.Commands;
+
+                    if (inLabel)
+                    {
+                        cmds = label.Commands;
+                    }
+
+                    var opline = line.Replace("\t", "").Split(' ');
+                    if (opline.Length > 1)
+                    {
+                        var opcmd = SyntaxNode.CreateCommand(opline[0], ParseArgs(line.Substring(opline[0].Length + 1)));
+                        cmds.Add(opcmd);
+                    }
+                    else
+                    {
+                        var opcmd = SyntaxNode.CreateCommand(opline[0], new List<AsmCommandArg>());
+                        cmds.Add(opcmd);
+                    }
                 }
             }
 
