8eed37e401b50d699d7e9089af1d89bb4cd140e0
Furesoft <filmee24@gmail.com>
2019-12-29 10:13:21 +0100

    cleanup

---
+++ Ref.Core/Parser/AsmLabel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Ref.Core.Parser
+{
+    public class AsmLabel
+    {
+        public List<AsmCommand> Commands { get; set; } = new List<AsmCommand>();
+
+        public string Name { get; set; }
+
+        public AsmLabel(string name)
+        {
+            Name = name;
+        }
+    }
+}
